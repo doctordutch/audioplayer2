5a007aa119b36d8df4ab06b9adf7cbda8f9dce6d
Justin VanHaven <vanhavenjustin@gmail.com>
2022-02-03 13:49:39 -0500

    first

---
+++ scripts/playlist.js
@@ -0,0 +1,19 @@
+trackList = [
+  {
+    name:"Happy Happy Joy Joy",
+    artist:"Ren & Stimpy",
+    src:"music/Happy Happy Joy Joy.mp3"
+  },
+          
+  {
+    name: "I Love Lucy",
+    artist:"TV Theme",
+    src: "music/I Love Lucy.mp3"
+  },
+          
+  {
+    name: "Rocky & Bullwinkle",
+    artist:"TV Theme",
+    src: "music/Rocky & Bullwinkle.mp3"
+  }
+];
